aee661a412c6a05f839231723f20af61d545cd16
Mauricio IC <mauricio_ic19@outlook.com>
2019-10-01 16:47:01 -0500

    added pokedex

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
-const pokedex = require('./pokedex.json').pokemon;
+const pokedex = require('./pokedex.json');
 
 app.get("/", (req, res) => {
-    res.send("¡Bienvenido al Pokédex!");
+    res.send("Bienvenido a mi Pokédex");
 });
 
 app.get("/pokemon", (req, res) => {
-    res.json(pokedex);
+    res.json(pokedex.pokemon);
 });
+
+app.get("/pokemon/:id", (req, res) => {
+    const id = req.params.id;
+    if (id > 0 && id <= 151) {
+        res.json(pokedex.pokemon[req.params.id - 1]);
+    }
+    else {
+        res.send("No hay ningún pokémon con ese ID en la base de datos");
+    }
+});
+
+app.get("/pokemon/:id/image", (req, res) => {
+    const img = pokedex.pokemon[req.params.id - 1].img;
+    res.send("<img src='"+img+"'>");
+})
 
 app.listen(3000, () => {
     console.log("Server is running...");

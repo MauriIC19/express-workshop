4385e5f5f8187177763fd8079944e65a6ceea221
Mauricio IC <mauricio_ic19@outlook.com>
2019-10-03 16:43:23 -0500

    added regex to routes

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ app.get("/pokemon", (req, res) => {
     res.json(pokedex.pokemon);
 });
 
-app.get("/pokemon/:id", (req, res) => {
+app.get("/pokemon/\\brandom\\b", (req, res) => {
+    res.send("Entraste a pokemon/random");
+})
+
+app.get("/pokemon/:name([A-Za-z]+)", (req, res) => {
+    const name = req.params.name;
+    const pokemon = pokedex.pokemon.filter((pokemon) => pokemon.name == name);
+    res.json(pokemon);
+});
+
+app.get("/pokemon/:id([0-9]{1,3})", (req, res) => {
     const id = req.params.id;
-    if (id > 0 && id <= 151) {
-        res.json(pokedex.pokemon[req.params.id - 1]);
-    }
-    else {
-        res.send("No hay ningún pokémon con ese ID en la base de datos");
-    }
+    res.json(pokedex.pokemon[id - 1]);
 });
 
 app.get("/pokemon/:id/image", (req, res) => {
     const img = pokedex.pokemon[req.params.id - 1].img;
-    res.send("<img src='"+img+"'>");
+    res.send("<img src='" + img + "'>");
 })
 
 app.listen(3000, () => {

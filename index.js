effc141e5c6d369f448a835c262092b8a4ea2519
Mauricio IC <mauricio_ic19@outlook.com>
2019-10-29 15:40:02 -0600

    added user route

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
-const notFoundHandler = require('./middleware/notFoundHandler');
-const corsHandler = require('./middleware/corsHandler');
+const notFound = require('./middleware/notFound');
+const cors = require('./middleware/cors');
 const app = express();
 
-app.use(corsHandler);
+app.use(cors);
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/pokemon", pokemon);
 app.use("/user", user);
 
-app.use(notFoundHandler);
+app.use(notFound);
 
 app.listen(3000, () => {
     console.log("Server is running...");

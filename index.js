f00c1cc840dacb2e75428001d4930c79c5122180
Mauricio IC <mauricio_ic19@outlook.com>
2019-11-07 13:58:18 -0600

    hola

--- index.js
+++ index.js
@@ -2,28 +2,22 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const pokemon = require('./routes/pokemon');
-const notFoundHandler = require('./middleware/notFoundHandler');
+const user = require('./routes/user');
+const notFound = require('./middleware/notFound');
+const cors = require('./middleware/cors');
+const auth = require('./middleware/auth');
 const app = express();
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/user", user);
+app.use(auth);
 app.use("/pokemon", pokemon);
-app.use(notFoundHandler);
+app.use(notFound);
 
 app.listen(3000, () => {
     console.log("Server is running...");
 });
-
-//const db = require('./config/database');
-// app.get("/test", (req, res) => {
-//     db.query("SELECT * FROM pokemon").then((rows)=>{
-//         res.status(200);
-//         res.send(rows);
-//     }).catch((err) => {
-//         res.status(500);
-//         res.send('Algo salió mal');
-//         console.log(err);
-//     });
-// });
